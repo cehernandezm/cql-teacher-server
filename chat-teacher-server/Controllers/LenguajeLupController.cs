8491264865c339cf1f13458f3df511d827e24056
Carlos <carloshernandez.chsegunda@gmail.com>
2019-08-11 19:26:58 -0600

    .

--- chat-teacher-server/Controllers/LenguajeLupController.cs
+++ chat-teacher-server/Controllers/LenguajeLupController.cs
@@ -2,41 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using chat_teacher_server.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace chat_teacher_server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class LenguajeLupController : ControllerBase
     {
-        // GET api/values
+        // GET: api/LenguajeLup
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET: api/LenguajeLup/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/LenguajeLup
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post(LenguajeLup codigo)
         {
+            return Ok("Hola");
         }
 
-        // PUT api/values/5
+        // PUT: api/LenguajeLup/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
---
+++ chat-teacher-server/Models/LenguajeLup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chat_teacher_server.Models
+{
+    public class LenguajeLup
+    {
+        public string codigo { get; set; }
+    }
+}
